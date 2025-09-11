c7198699f71a994e93073bb9437656f44231bfa1
Robert He <hnrobert@qq.com>
2025-09-12 01:51:24 +0800

    refactor: remove Parallax component for simpler hobby card layout

--- src/components/hobbies/HobbyCardWithoutImage.tsx
+++ src/components/hobbies/HobbyCardWithoutImage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { Parallax } from "react-scroll-parallax";
 import { HobbyData } from "./types";
 
 interface HobbyCardWithoutImageProps {
@@ -18,20 +17,10 @@ export const HobbyCardWithoutImage: React.FC<HobbyCardWithoutImageProps> = ({
       className="hobby-card-without-image"
       data-hobby-id={`hobby-no-image-${index}`}
     >
-      <Parallax
-        translateY={[-30, 30]}
-        opacity={[0, 1]}
-        easing="easeOut"
-        speed={-2}
-        shouldAlwaysCompleteAnimation={true}
-        disabled={false}
-        style={{ width: "100%" }}
-      >
-        <div className="hobby-item-simple">
-          <span className="hobby-emoji-simple">{hobby.emoji}</span>
-          <span className="hobby-label-simple">{hobby.name}</span>
-        </div>
-      </Parallax>
+      <div className="hobby-item-simple">
+        <span className="hobby-emoji-simple">{hobby.emoji}</span>
+        <span className="hobby-label-simple">{hobby.name}</span>
+      </div>
     </div>
   );
 };
